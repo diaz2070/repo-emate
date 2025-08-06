d8560b5ea255d6a58608062d789d902b4addcf6c
Andres Diaz <andres.diaz2070@gmail.com>
2025-08-05 19:47:36 -0600

    chore(linter): add linter, prettier and husky to dev dependencies - Moved app folder to src directory for cleaner view

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +12,26 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    ignores: [
+      '.next/**',
+      'node_modules/**',
+      'dist/**',
+      'build/**',
+      'coverage/**',
+    ],
+  },
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  ...compat.extends('plugin:prettier/recommended'),
+  ...tseslint.config(
+    eslint.configs.recommended,
+    tseslint.configs.recommended,
+    tseslint.configs.strict,
+    tseslint.configs.stylistic,
+  ),
+  {
+    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+  },
 ];
 
 export default eslintConfig;
